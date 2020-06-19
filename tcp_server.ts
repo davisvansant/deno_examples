ce4ecc316068d947dd2922b7b02ae6dec3240817
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-19 13:36:01 -0700

    tcp server

---
+++ tcp_server.ts
@@ -0,0 +1,9 @@
+const hostname = "0.0.0.0";
+const port = 8080;
+const listener = Deno.listen({ hostname, port });
+
+console.log(`Listening on ${hostname}:${port}`);
+
+for await (const connection of listener) {
+  Deno.copy(connection, connection);
+}
